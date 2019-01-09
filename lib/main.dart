0c865fad3c476a2c1de9562396f7297d52134f86
LindaKadz <achielinda44@gmail.com>
2019-01-09 12:24:51 +0300

    registration details are saved and displayed on the console

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:validate/validate.dart';
+import 'package:validate/validate.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,10 +15,19 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text(appTitle),
         ),
-        body: RegistrationForm(),
+        body: RegistrationForm(),    
       ),
     );
   }
+}
+
+class _RegistrationData {
+  String name = '';
+  String email = '';
+  String nickname = '';
+  String password = '';
+  String confirmPassword = '';
+
 }
 
 class RegistrationForm extends StatefulWidget {
@@ -30,6 +39,40 @@ class RegistrationForm extends StatefulWidget {
 
 class RegistrationFormState extends State <RegistrationForm> {
   final _formKey = GlobalKey<FormState> ();
+  _RegistrationData _data = new _RegistrationData();
+
+  String _validateEmail(String value) {
+    try {
+      Validate.isEmail(value);
+    } catch (e) {
+      return 'The E-mail Address must be a valid email address.';
+    }
+
+    return null;
+  }
+
+  String _validatePassword(String value) {
+    if (value.length < 8) {
+      return 'The Password must be at least 8 characters.';
+    }
+
+    return null;
+  }
+
+
+  void submit() {
+    if (this._formKey.currentState.validate()) {
+      _formKey.currentState.save(); 
+
+      print('Printing the registration data.');
+      print('Name: ${_data.name}');
+      print('Email: ${_data.email}');
+      print('Nickname: ${_data.nickname}');
+      print('Password: ${_data.password}');
+      print('Confirm Password: ${_data.confirmPassword}');
+    }
+}
+
   @override
 
   Widget build(BuildContext context) {
@@ -47,18 +90,22 @@ class RegistrationFormState extends State <RegistrationForm> {
                 return 'Please fill this section';
               }
             },
+            onSaved: (String value) {
+              this._data.name = value;
+            },
           ),
+
           TextFormField(
             keyboardType: TextInputType.emailAddress,
             decoration: InputDecoration(
               labelText: 'Email'
             ),
-            validator: (String value) {
-              if (value.isEmpty) {
-                return 'Please fill this section';
-              }
+            validator: this._validateEmail,
+            onSaved: (String value) {
+              this._data.email = value;
             },
           ),
+
           TextFormField(
             decoration: InputDecoration(
               labelText: 'Nickname'
@@ -68,38 +115,37 @@ class RegistrationFormState extends State <RegistrationForm> {
                 return 'Please fill this section';
               }
             },
+            onSaved: (String value) {
+              this._data.nickname = value;
+            },
           ),
+
           TextFormField(
             obscureText: true,
             decoration: InputDecoration(
               labelText: 'Password'
             ),
-            validator: (String value) {
-              if (value.isEmpty) {
-                return 'Please fill this section';
-              }
+            validator: this._validatePassword,
+            onSaved: (String value) {
+              this._data.password = value;
             },
           ),
+
           TextFormField(
             obscureText: true,
             decoration: InputDecoration(
               labelText: 'Confirm Password'
             ),
-            validator: (String value) {
-              if (value.isEmpty) {
-                return 'Please fill this section';
-              }
+            validator: this._validatePassword,
+            onSaved: (String value) {
+              this._data.confirmPassword = value;
             },
           ),
+
         Padding(
             padding: const EdgeInsets.symmetric(vertical: 16.0),
             child: RaisedButton(
-              onPressed: () {
-                if (_formKey.currentState.validate()) {
-                  Scaffold.of(context)
-                      .showSnackBar(SnackBar(content: Text('Creating Profile...')));
-                }
-              },
+              onPressed: this.submit,
               child: Text('Register'),
             ),
           ),
